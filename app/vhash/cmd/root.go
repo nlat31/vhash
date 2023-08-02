f8cd60d221b6de1d568ba4f116617bf01bbe0a0f
Your Name <you@example.com>
2023-08-02 12:42:04 +0800

    0

--- app/vhash/cmd/root.go
+++ app/vhash/cmd/root.go
@@ -14,8 +14,10 @@ import (
 )
 
 var rootContext struct {
-	hash string
-	show bool
+	hash    string
+	show    bool
+	reverse bool
+	max     int64
 }
 
 func hashMethod(hashName string) hash.Hash {
@@ -35,47 +37,87 @@ var rootCmd = &cobra.Command{
 	Short: "",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("请输入哈希次数执行:")
-		text, err := term.ReadPassword(int(os.Stdin.Fd()))
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		if rootContext.show {
-			fmt.Println(string(text))
-		}
-		count, err := strconv.ParseInt(string(text), 10, 64)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		if count < 1 {
-			fmt.Println("哈希次数必须大于0")
-			return
-		}
-		fmt.Println("请输入文本:")
-		text, err = term.ReadPassword(int(os.Stdin.Fd()))
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		if rootContext.show {
-			fmt.Println(string(text))
-		}
-		for i := int64(0); i < count; i++ {
-			alg := hashMethod(rootContext.hash)
-			if alg == nil {
-				return
-			}
-			_, err = alg.Write(text)
+		if !rootContext.reverse {
+			fmt.Println("请输入哈希次数执行:")
+			text, err := term.ReadPassword(int(os.Stdin.Fd()))
 			if err != nil {
 				fmt.Println(err.Error())
 				return
 			}
-			h := alg.Sum(nil)
-			text = []byte(hex.EncodeToString(h))
+			if rootContext.show {
+				fmt.Println(string(text))
+			}
+			count, err := strconv.ParseInt(string(text), 10, 64)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			if count < 1 {
+				fmt.Println("哈希次数必须大于0")
+				return
+			}
+			fmt.Println("请输入文本:")
+			text, err = term.ReadPassword(int(os.Stdin.Fd()))
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			if rootContext.show {
+				fmt.Println(string(text))
+			}
+			for i := int64(0); i < count; i++ {
+				alg := hashMethod(rootContext.hash)
+				if alg == nil {
+					return
+				}
+				_, err = alg.Write(text)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				h := alg.Sum(nil)
+				text = []byte(hex.EncodeToString(h))
+			}
+			fmt.Println(string(text))
+		} else {
+			fmt.Println("请输入文本:")
+			text, err := term.ReadPassword(int(os.Stdin.Fd()))
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			if rootContext.show {
+				fmt.Println(string(text))
+			}
+			fmt.Println("请输入哈希:")
+			hashVal, err := term.ReadPassword(int(os.Stdin.Fd()))
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			if rootContext.show {
+				fmt.Println(string(hashVal))
+			}
+			times := int64(1)
+			for ; times < rootContext.max; times++ {
+				alg := hashMethod(rootContext.hash)
+				if alg == nil {
+					return
+				}
+				_, err = alg.Write(text)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				h := alg.Sum(nil)
+				hashStr := hex.EncodeToString(h)
+				if hashStr == string(hashVal) {
+					break
+				}
+				text = []byte(hashStr)
+			}
+			fmt.Println(times)
 		}
-		fmt.Println(string(text))
 	},
 }
 
@@ -89,4 +131,6 @@ func Execute() {
 func init() {
 	rootCmd.Flags().StringVar(&rootContext.hash, "hash", "md5", "使用的哈希算法(sha256, md5)")
 	rootCmd.Flags().BoolVar(&rootContext.show, "show", false, "显示输入内容")
+	rootCmd.Flags().BoolVarP(&rootContext.reverse, "reverse", "r", false, "反向计算哈希次数")
+	rootCmd.Flags().Int64VarP(&rootContext.max, "max", "m", 1000000, "反向计算哈希时的最大值")
 }
